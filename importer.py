6e68d93b1db0754b1b3770da72023320bc83f3fa
J. Heasly <jheasly@earthlink.net>
2013-09-27 18:02:27 -0700

    Tree navigation basics figured.

--- importer.py
+++ importer.py
@@ -1,12 +1,22 @@
-import feedparser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lxml import objectify, etree
 
 loc = 'ORC039'
 
 def main():
     feed_url = 'http://alerts.weather.gov/cap/wwaatmget.php?x=%s' % (loc)
-    print feed_url
-#     feed = feedparser.parse(feed_url)
-#     for entry in feed.entries:
-#         print entry
+    
+    doc = objectify.parse(feed_url)
+    tree = doc.getroot()
+    
+#     print(objectify.dump(tree))
+    
+    for e in tree.entry:
+#         print e.title
+#         print e.summary
+        print 'What: %s' % e['{urn:oasis:names:tc:emergency:cap:1.1}event']
+        print 'Expires: %s' % e['{urn:oasis:names:tc:emergency:cap:1.1}expires']
 
 if __name__ == "__main__" : main()
